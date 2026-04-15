00cc8d72c0fb210ae936f4b1fa8b719bc1a8577c
Josh Archer <jarcher@greenriver.edu>
2026-04-15 09:03:55 -0700

    Added stubs for a new ArrayList class.

--- Array List/src/list/ArrayList.java
+++ Array List/src/list/ArrayList.java
@@ -2,8 +2,10 @@ package list;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
-public class ArrayList<T> implements Collection<T> {
+public class ArrayList<T> implements Collection<T>, List<T> {
     private T[] data;
     private int size;
 
@@ -35,6 +37,51 @@ public class ArrayList<T> implements Collection<T> {
 
     }
 
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return List.of();
+    }
+
     public Iterator<T> iterator() {
         return null;
     }
@@ -52,6 +99,11 @@ public class ArrayList<T> implements Collection<T> {
     }
 
     public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
         return false;
     }
 
